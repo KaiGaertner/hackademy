2edf49261775f09abd2012996b2ed9c781a74938
Christian Dohnert <donut87@gmail.com>
2019-03-03 12:47:49 +0100

    Wait for edge eliminates the need for sleeping

--- raspberry3B+/control.py
+++ raspberry3B+/control.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 
 from blinzel import Blinzler
-import time
 import RPi.GPIO as GPIO
 
 # RPi.GPIO Layout verwenden (wie Pin-Nummern)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(40, GPIO.IN) # Escape
 GPIO.setup(12, GPIO.IN) # Play
-#GPIO.setwarnings(False)
+# GPIO.setwarnings(False)
 blinzler = Blinzler()
 read = True
 # say hello to user
 # subprocess.call(['aplay', 'sounds/hello.wav'])
 blinzler.play_audio('sounds/hello.wav')
 while True:
-	if GPIO.input(40)==GPIO.LOW:
-		# sleep befehl zum entprellen
-		time.sleep(2)
-		blinzler.take_picture()
-		blinzler.make_ocr()
-		blinzler.read_ocr()
-		blinzler.speak_text()		
-		print("Lesen beendet")
-	
-	time.sleep(0.3)
+	GPIO.wait_for_edge(40, GPIO.FALLING)
+	blinzler.take_picture()
+	blinzler.make_ocr()
+	blinzler.read_ocr()
+	blinzler.speak_text()
+	print("Lesen beendet")
+
