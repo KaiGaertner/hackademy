94d37d692ee4fec70771616df4f66bfa11dfb1ba
Diego Penilla <jdchipox@gmail.com>
2019-03-09 12:15:37 +0100

    empty directory

--- raspberry3B+/rotate.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-
-def pil_rotate(name):
-	im = Image.open(name)
-	im.rotate(90).save(name.replace('.png', '_rotate.png'))
-
-
-pil_rotate("test.png")
--- vnc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-vncserver :0 -geometry 1920x1080 -depth 24 -dpi 96
