1d920e3012de20f03b573a6990394370c657a675
Diego Penilla <jdchipox@gmail.com>
2019-03-12 22:55:23 +0100

    updated image with working audio and voice instructions

--- control.py
+++ control.py
@@ -1,6 +1,8 @@
 import argparse
 import time, os, subprocess
 import RPi.GPIO as GPIO
+import wave, sys, pyaudio 
+
 
 # Controls how to configure raspberry pins: Add more modes?
 ap = argparse.ArgumentParser()
@@ -8,31 +10,57 @@ ap.add_argument("-s", "--scan", default=True,
         help = "Execute image reader")
 ap.add_argument("-r", "--reader", default=False,
         help = "Execute image reader")
+ap.add_argument('-a', '--ausgabe', help = 'How to convert text to speech')
 args = vars(ap.parse_args())
 
-# _ _ _ _ _ _ _  _ _ _ _ _ _ _  _ _ _  _
-# Button pin 40
+
+# Audio
+def create_play(text, speed=0.93, language='de-DE'):
+    '''transforms given text into a temporary wav file and plays it. 
+       text: words to be spoken
+       speed: changes rate of sound [0-1]
+       language: choose language see aplay -h'''
+
+    os.system('''pico2wave -l de-DE -w /home/pi/hackdemyk/audio/temp.wav "{}"'''.format(text))
+    sound = wave.open("/home/pi/hackdemyk/audio/temp.wav")
+    p = pyaudio.PyAudio()
+    chunk = 1024
+    stream = p.open(format =
+                p.get_format_from_width(sound.getsampwidth()),
+                channels = sound.getnchannels(),
+                rate = int(sound.getframerate()*speed),
+                output = True)
+    data = sound.readframes(chunk)
+    while True:
+        if data != '':
+            stream.write(data)
+            data = sound.readframes(chunk)
+        if data == b'':
+            break
+
+# Initializing raspberrypi 3b + pins.
+
+create_play(text='Das System is bereit. Drucken Sie auf den linken Knopf um Bilder aufzunehmen. Wenn sie fertig sind, drucken sie auf den rechten Knopf um die Textverarbeitung zu starten')
 if bool(args['scan']) == True:
     # RPi.GPIO Layout verwenden (wie Pin-Nummern)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) # When Button pressed, voltage received, taking a photo
+    GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP) # When Button pressed, voltage received, taking a photo
     GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     read = True
-    #blinzler.play_audio('sounds/hello.wav')
-    print('Press Button@Pin12 to take a picture')
-    print("Press Button@Pin11 to take a picture")
     count = 0
     while True:
-        if GPIO.input(12) == False:
-            # Execute Code
-            os.system('python3 camera.py -n hackdemyk/scanned_images/DEBUG{}'.format(count))
+        if GPIO.input(13) == False:
+            create_play(text='Bild wird aufgenommen')
+            os.system('python3 camera.py -n DEBUG{}'.format(count))
             count+=1
             time.sleep(2)
-            print("Ready to take  another picture")
+            print("Press Button@13 to take a picture")
+            create_play(speed=0.93, text='Drucken Sie erneut den gleichen Knopf um ein neues Bild aufzunehmen. Oder drucken Sie den rechten Knopf um Verarbeitung zu starten')
             print("Or Press Button@Pin11 to Extract Text")
         if GPIO.input(11) == False:
             print('Initiating Text Extraction')
-            mode='online'
+            mode='offline'
+            create_play(text='Verarbeitung gestartet')
             os.system("python3 images_to_text.py -f scanned_images -m {} -o results".format(mode))
             break
 print('Completed')
