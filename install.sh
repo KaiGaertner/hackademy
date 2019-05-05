0b34ca5531ae7d8841a34337c3eb78eb7ab749f5
KaiGaertner <post@kaigaertner.de>
2019-05-05 19:18:20 +0100

    added dependencies for jpgdev libraries

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt install --assume-yes python3 python3-pip python3-dev libopenjp2-7 libtiff5 tesseract-ocr tesseract-ocr-deu portaudio19-dev
+sudo apt install --assume-yes python3 python3-pip python3-dev libopenjp2-7 libtiff5 tesseract-ocr tesseract-ocr-deu portaudio19-dev libjpeg-dev zlib1g-dev
 
 pip3 install pillow picamera RPi.GPIO pyaudio
