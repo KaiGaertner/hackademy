d1ca07549f7b085ac77cec19cdb5e7b98eb1781f
KaiGaertner <post@kaigaertner.de>
2019-05-05 19:24:35 +0100

    added pytesseract to PIL install

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 sudo apt install --assume-yes python3 python3-pip python3-dev libopenjp2-7 libtiff5 tesseract-ocr tesseract-ocr-deu portaudio19-dev libjpeg-dev zlib1g-dev
 
-pip3 install pillow picamera RPi.GPIO pyaudio
+pip3 install pillow picamera RPi.GPIO pyaudio pytesseract
